80bfb1d8ce937a0a3ed8be642688b58a9e8cf97b
soe43 <soe433@gmail.com>
2016-12-15 03:37:04 -0500

    Added bubbleSort.

--- 04Sort/Sorts.java
+++ 04Sort/Sorts.java
@@ -42,16 +42,42 @@ public class Sorts{
 	}
     }
 
-    
+    /**Bubble sort of an int array.
+     *Upon commpletion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void bubbleSort(int[] data){
+	
+
+	for(int i = 0; i < data.length-1;i++){
+	    int counter = 0;
+	    if(data[i] > data[i+1]){
+		counter++;
+		int temp = data[i];
+		data[i] = data[i+1];
+		data[i+1] = temp;
+	    }
+	}
+	    if(counter == 0){
+		return;
+	    }
+	    else{
+		counter = 0;
+	    }
+    }
 
     public static void main(String[]args){
 	name();
 	int[] yo = new int[] {64, 25, 12, 22, 11};
-	int[] bo = new int[] {32, 456, 34, 81, 13, 123}; 
+	int[] bo = new int[] {32, 456, 34, 81, 13, 123};
+	int[] bub = new int[] {123, 1234, 12347, 1231, 301, 54, 15, 96};
 	selectionSort(yo);
 	System.out.println(Arrays.toString(yo));
 	insertionSort(bo);
 	System.out.println(Arrays.toString(bo));
+	bubbleSort(bub);
+	System.out.println(Arrays.toString(bub));
+
     }
     
 }
